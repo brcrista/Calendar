4b306c0b9b37e6266840e0cd10392d9d62a7fecf
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-06-20 16:24:33 -0400

    Add tests for more endpoints

--- test/functional/calendar-api.test.js
+++ test/functional/calendar-api.test.js
@@ -1,21 +1,43 @@
 // Prerequisite: deploy the Calendar API service
 
 const http = require('http');
-const url = require('url');
+const URL = require('url').URL;
 
-const urlBase = 'http://localhost:1498';
+const apiRoot = 'http://localhost:1498/api/v1/';
 
-describe('the users API', () => {
+/**
+ * Call an endpoint and check its status code in a Jest test.
+ * @param {string} endpoint - Path of the endpoint to test
+ * @param {number} statusCode - Expected HTTP status code in the response
+ */
+function testEndpoint(endpoint, statusCode, done) {
+    const url = new URL(endpoint, apiRoot);
+    http.get(url, (res) => {
+        try {
+            expect(res.statusCode).toStrictEqual(statusCode);
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+}
+
+describe('The users endpoint', () => {
+    test('does not return all users', (done) => {
+        testEndpoint('users', 404, done);
+    });
+
     test('returns 200 for an existing user', (done) => {
-        http.get(
-            new url.URL('api/v1/users/1', urlBase),
-            (res) => {
-                try {
-                    expect(res.statusCode).toBe(200);
-                    done();
-                } catch (error) {
-                    done(error);
-                }
-        });
+        testEndpoint('users/1', 200, done);
+    });
+});
+
+describe('The events endpoint', () => {
+    test('does not return all events', (done) => {
+        testEndpoint('events', 404, done);
+    });
+
+    test('returns 200 for an existing event', (done) => {
+        testEndpoint('events/1', 200, done);
     });
 });
