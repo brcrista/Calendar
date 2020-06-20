022c1821da3217ef00b0440dc59ba48502f0ad4c
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-06-20 12:52:29 -0400

    Sealed classes

--- src/ApiService/Program.cs
+++ src/ApiService/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Calendar.ApiService
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
