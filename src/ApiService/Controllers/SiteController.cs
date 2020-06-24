886dc165fe1302807fbe7c8d23d91227823255aa
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-06-23 21:14:55 -0400

    Implement the remaining API endpoints (#4)
    * Add doc comments
    
    * Add _site endpoints
    
    * Add endpoint for a user's events
    
    * Add functional tests for the user events endpoint
    
    * Make the fetch helper more general

---
+++ src/ApiService/Controllers/SiteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calendar.ApiService.Controllers
+{
+    /// <summary>
+    /// Provides access to information about the site.
+    /// </summary>
+    [Route("_site")]
+    [ApiController]
+    public sealed class SiteController : ControllerBase
+    {
+        /// <summary>
+        /// Tells whether the site is online.
+        /// </summary>
+        [HttpGet("live")]
+        public IActionResult Live() => Ok();
+
+        /// <summary>
+        /// Tells the UTC time of when the site was deployed.
+        /// </summary>
+        [HttpGet("version")]
+        public DateTime Version() => deployedAt;
+
+        private static readonly DateTime deployedAt = DateTime.UtcNow;
+    }
+}
--- test/functional/calendar-api.test.js
+++ test/functional/calendar-api.test.js
@@ -8,12 +8,13 @@ const apiRoot = 'http://localhost:1498/api/v1/';
 /**
  * Call a URL and return the response's status code.
  * @param {URL} url - URL to call for a status code
+ * @returns {Promise} - the HTTP response message
  */
-function fetchStatusCode(url) {
+function fetch(url) {
     return new Promise((resolve, reject) => {
         http.get(url, (res) => {
             try {
-                resolve(res.statusCode);
+                resolve(res);
             } catch (error) {
                 reject(error);
             }
@@ -24,27 +25,45 @@ function fetchStatusCode(url) {
 describe('The users endpoint', () => {
     test('returns 404 for all users', async () => {
         const url = new URL('users', apiRoot);
-        const statusCode = await fetchStatusCode(url);
-        expect(statusCode).toStrictEqual(404);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(404);
     });
 
     test('returns 200 for an existing user', async () => {
         const url = new URL('users/1', apiRoot);
-        const statusCode = await fetchStatusCode(url);
-        expect(statusCode).toStrictEqual(200);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(200);
+    });
+
+    test("can get an existing user's events", async () => {
+        const url = new URL('users/1/events', apiRoot);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(200);
+    });
+
+    test("can filter a user's events with query parameters", async () => {
+        const url = new URL('users/1/events?hostId=2&hasAccepted=false', apiRoot);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(200);
+    });
+
+    test('rejects invalid query parameters', async () => {
+        const url = new URL('users/1/events?hostId=hello', apiRoot);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(400);
     });
 });
 
 describe('The events endpoint', () => {
     test('returns 404 for all events', async () => {
         const url = new URL('events', apiRoot);
-        const statusCode = await fetchStatusCode(url);
-        expect(statusCode).toStrictEqual(404);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(404);
     });
 
     test('returns 200 for an existing event', async () => {
         const url = new URL('events/1', apiRoot);
-        const statusCode = await fetchStatusCode(url);
-        expect(statusCode).toStrictEqual(200);
+        const response = await fetch(url);
+        expect(response.statusCode).toStrictEqual(200);
     });
 });
