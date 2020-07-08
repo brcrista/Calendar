e58535d89044574c02aefb2bbe58ebc3eebcd8bf
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-08 19:22:55 -0400

    Add seed.sql and some Makefile targets

---
+++ test/seed.sql
@@ -0,0 +1,31 @@
+-- Create 4 accounts and 4 users
+INSERT INTO Accounts (id, email, password)
+VALUES (1, 'brian@email.com', '12345678');
+
+INSERT INTO Accounts (id, email, password)
+VALUES (2, 'deirdre@email.com', 'p@ssw0rd!');
+
+INSERT INTO Accounts (id, email, password)
+VALUES (3, 'caitie3@email.com', 'correcthorsebatterystaple');
+
+INSERT INTO Accounts (id, email, password)
+VALUES (4, 'matthew@email.com', 'user4@email.com');
+
+INSERT INTO Users (id, display_name, account_id)
+VALUES (1, 'Brian', 1);
+
+INSERT INTO Users (id, display_name, account_id)
+VALUES (2, 'Deirdre', 2);
+
+INSERT INTO Users (id, display_name, account_id)
+VALUES (3, 'Caitie', 3);
+
+INSERT INTO Users (id, display_name, account_id)
+VALUES (4, 'Matthew', 4);
+
+-- 2 events
+INSERT INTO Events (id, title, start, end, location, owner_id)
+VALUES (1, 'Brian''s Party', '2020-08-31 00:00', '2020-08-31 04:00', '123 Main St, Raleigh, NC, 27603', 1);
+
+INSERT INTO Events (id, title, start, end, location, owner_id)
+VALUES (2, 'Christmas UTC', '2020-12-25 00:00', '2020-12-25 23:59', NULL, 1);
