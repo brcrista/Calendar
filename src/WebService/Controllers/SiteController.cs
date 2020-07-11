09e1557cc3f5274146bd4e4ff27945b1811d7c1d
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-11 08:41:24 -0400

    add EventController

--- src/WebService/Controllers/SiteController.cs
+++ src/WebService/Controllers/SiteController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace Calendar.WebService.Controllers
---
+++ src/WebService/Controllers/View/EventController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calendar.WebService.Controllers.View
+{
+    [Controller]
+    [Route("events/{id}")]
+    public sealed class EventController : Controller
+    {
+        public IActionResult GetEvent(long id)
+        {
+            return View();
+        }
+    }
+}
