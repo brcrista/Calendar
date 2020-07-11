b1ed38d0ee5ce3e78cc5bdc3c13974d9a6c21129
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-07-11 11:40:08 -0400

    Fill in the User and Event models in their views

--- src/WebService/Controllers/View/UserController.cs
+++ src/WebService/Controllers/View/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Calendar.ObjectModel.DataProviders;
 
 namespace Calendar.WebService.Controllers.View
 {
@@ -6,10 +10,25 @@ namespace Calendar.WebService.Controllers.View
     [Route("users/{id}")]
     public sealed class UserController : Controller
     {
-        public IActionResult GetUser(long id)
+        private readonly IUsersProvider usersProvider;
+
+        public UserController(IUsersProvider usersProvider)
         {
-            ViewBag.Title = "DISPLAY_NAME's profile";
-            return View();
+            this.usersProvider = usersProvider;
+        }
+
+        public async Task<IActionResult> GetUser(long id)
+        {
+            var user = await usersProvider.GetUserAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                ViewBag.Title = $"{user.DisplayName}'s profile";
+                return View(user);
+            }
         }
     }
 }
